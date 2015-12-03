0d32e22fc1d85e4ed5649881dc23d9f49be13b23
G. Kay Lee <balancetraveller+github@gmail.com>
2015-12-03 16:40:54 +0800

    Update helpers to use the latest Frontier structure

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -9,11 +9,9 @@
 // ### NPM Modules
 
 import 'babel-core/polyfill';
-import {keys} from 'bound-native-methods/object';
 import React from 'react'; // Required by JSX
 import {render} from 'react-dom';
-import {applyMiddleware, combineReducers, createStore} from 'redux';
-import ReduxThunk from 'redux-thunk';
+import {applyMiddleware, createStore, combineReducers} from 'redux';
 import ReduxPromise from 'redux-promise';
 import createLogger from 'redux-logger';
 import {Provider} from 'react-redux';
@@ -21,35 +19,45 @@ import {Provider} from 'react-redux';
 // ### Local Modules
 
 import {defaultState} from 'scripts/configs';
+import {reduceToObject} from 'scripts/helpers';
 import * as reducers from 'scripts/reducers';
 import App from 'scripts/containers/App';
 
 // Configure Redux Store
 // ---------------------
 //
+// > configureStore : Function
+//
+// (Reducer, ?State) -> (
+//   StoreCreator
+//   -> StoreEnhancer: StoreCreator
+//   -> StoreEnhancer: StoreCreator
+// ): Store
+//
 // Apply the following [Redux middleware](https://github.com/rackt/redux/blob/master/docs/advanced/Middleware.md):
 //
-// * [Redux Thunk](https://github.com/gaearon/redux-thunk) - Enable function-based [async actions](https://github.com/rackt/redux/blob/master/docs/advanced/AsyncActions.md)
 // * [Redux Promise](https://github.com/acdlite/redux-promise) - Enable promise-based async actions
 // * [Redux Logger](https://github.com/fcomb/redux-logger) - Add action logger
 
 function configureStore (initialState) {
   return applyMiddleware(
-    ReduxThunk,
     ReduxPromise,
     createLogger()
-  )(createStore)(
-
+  )(
+    createStore
+  )(
     // ### Create the Root Reducer
     //
     // Create the root reducer by combining meta-reducers, which are formed by organizing reducers according to the shape of the state tree.
+    //
+    // Note that the initial value of the `reduce()` call is not an empty object, but contains `formReducer` from [Redux Form](https://github.com/erikras/redux-form), with `reducers.form` being plugged into it.
+    //
+    // Do ***not*** pass `reducers.form` directly into `plugin()` because `reducers.form` contains a `default` property not intended for consumption.
+    //
+    // See also https://github.com/babel/babel/issues/2544.
 
     combineReducers(
-      defaultState::keys().reduce((metareducers, stateName) => {
-        metareducers[stateName] = combineReducers(reducers[stateName]);
-
-        return metareducers;
-      }, {})
+      defaultState::reduceToObject((_, stateName) => combineReducers(reducers[stateName]))
     ),
 
     initialState
